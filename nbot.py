c058abba24e76f833bd8b09ab863ef69670a3778
Yalchin403 <yalchin.mammadli.std@bhos.edu.az>
2021-03-16 18:36:27 +0400

    on_message added to main file to fix check-rank channel problem

--- nbot.py
+++ nbot.py
@@ -4,6 +4,7 @@ import asyncpg
 from dotenv import load_dotenv
 from discord.ext import commands
 load_dotenv()
+from bad_words import polished_data
 
 intents = discord.Intents.default()
 intents.members = True
@@ -14,20 +15,67 @@ database = os.getenv('database')
 user = os.getenv('user')
 password = os.getenv('password')
 
+@client.event
+async def on_message(message):
+    if message.content != "./rank" and message.channel.id == 817409301479948338:
+        try:
+            await message.author.send(f"You can only check your rank in {message.channel.mention}.")
+            await message.delete()
+        except:
+            pass
+    else:
+        await client.process_commands(message)
+        if "discord.gg/" in message.content.lower() or "discordapp.com/invite/" in message.content.lower():
+            await message.author.send("sending server invites breaks the rules, if you think this is a mistake please contact the moderator.")
+            await message.delete()
+        
+        message_list = message.content.lower().split(' ')
+        for word in polished_data:
+            if word in message_list:
+                await message.author.send("sending swear words breaks the rules, if you think this is a mistake please contact the moderator.")
+                await message.delete()
+                break
+        try:
+            user = message.author
+            user_id = str(message.author.id)
+            guild_id = str(message.guild.id)
+            channel_ids = [813164947848167464, 814936857300566066]
+        except:
+            pass
+        if not user.bot and message.channel.id not in channel_ids:
+            
+            user = await client.pg_con.fetch("SELECT * FROM user_levels WHERE user_id = $1 AND guild_id = $2", user_id, guild_id)
+            if not user:
+                await client.pg_con.execute("INSERT INTO user_levels (user_id, guild_id, exp, lvl) VALUES ($1, $2, 0, 1)", user_id, guild_id)
+                embed = discord.Embed(title="You Leveled Up", description=f"{message.author.mention} your level is 1", color=discord.Color.purple())
+                await message.channel.send(embed=embed)
+            else:
+                user = await client.pg_con.fetchrow("SELECT * FROM user_levels WHERE user_id = $1 AND guild_id = $2", user_id, guild_id)
+                exp = user['exp'] + 5
+                current_lvl = user['lvl']
+                new_lvl = int(exp ** 0.25)
+                if current_lvl < new_lvl:
+                    await client.pg_con.execute("UPDATE user_levels SET exp = $1 WHERE user_id = $2 AND guild_id = $3", exp, user_id, guild_id)
+                    await client.pg_con.execute("UPDATE user_levels SET lvl = $1 WHERE user_id = $2 AND guild_id = $3", new_lvl, user_id, guild_id)
+                    embed = discord.Embed(title="You Leveled Up", description=f"{message.author.mention} your level is {new_lvl}", color=discord.Color.purple())
+                    await message.channel.send(embed=embed)
+                else:
+                    await client.pg_con.execute("UPDATE user_levels SET exp = $1 WHERE user_id = $2 AND guild_id = $3", exp, user_id, guild_id)
+
 async def create_db_pool():
     client.pg_con = await asyncpg.create_pool(database=database, user=user, password=password, host="127.0.0.1")
 
 @client.command()
 async def load(ctx, extension):
-	client.load_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
 
 @client.command()
 async def unload(ctx, extension):
-	client.unload.extension(f'cogs.{extension}')
+    client.unload.extension(f'cogs.{extension}')
 
 for filename in  os.listdir('./cogs'):
-	if filename.endswith('.py'):
-		client.load_extension(f'cogs.{filename[:-3]}')
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
 client.loop.run_until_complete(create_db_pool())
 TOKEN = os.getenv('TOKEN')

36d34d792a7f680ef3471f081369803909f301f6
Yalchin403 <yalchin.mammadli.std@bhos.edu.az>
2021-03-17 22:06:33 +0400

    rank command edited to show a bit nicer embed by displaying level, rank and xp

--- cogs/rank.py
+++ cogs/rank.py
@@ -15,11 +15,17 @@ class RemoveCog(commands.Cog):
         if user:
             user = await self.client.pg_con.fetchrow("SELECT * FROM user_levels WHERE user_id = $1 AND guild_id = $2", user_id, guild_id)
             level = user['lvl']
-            embed = discord.Embed(title="Level Check", description=f"{member.mention}, your level is {level}", color=discord.Color.purple())
+            all_users = await self.client.pg_con.fetch('SELECT * FROM user_levels ORDER BY lvl DESC')
+            rank = all_users.index(user)
+            embed = discord.Embed(color=member.color, timestamp=ctx.message.created_at)
+            embed.set_author(name=f"Level - {member}",icon_url=member.avatar_url)
+            embed.add_field(name="Level", value=level)
+            embed.add_field(name="RANK", value=rank+1)
+            embed.add_field(name="XP", value=user['exp'])
             await ctx.message.channel.send(embed=embed)
         else:
             level = 1
-            embed = discord.Embed(title="Levelled Check", description=f"{member.mention}, your levelled is {level}", color=discord.Color.purple())
+            embed = discord.Embed(title="Levelled Check", description=f"{member.mention}, your levelled is {level}", color=member.color)
             await ctx.message.channel.send(embed=embed)
     
 def setup(client):

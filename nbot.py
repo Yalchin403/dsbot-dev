9b2643fa956eeb6aa0fe5ea7b727f0f8d2a7f2ab
Yalchin403 <yalchin.mammadli.std@bhos.edu.az>
2021-03-17 22:10:01 +0400

    anti spam added

--- nbot.py
+++ nbot.py
@@ -5,19 +5,21 @@ from dotenv import load_dotenv
 from discord.ext import commands
 load_dotenv()
 from bad_words import polished_data
+from AntiSpam import AntiSpamHandler
 
 intents = discord.Intents.default()
 intents.members = True
 intents.reactions = True
 command_prefix = os.getenv('command_prefix')
 client = commands.Bot(command_prefix = command_prefix, intents = intents, help_command=None)
+client.handler = AntiSpamHandler(client)
 database = os.getenv('database')
 user = os.getenv('user')
 password = os.getenv('password')
 
 @client.event
 async def on_message(message):
-    if message.content != "./rank" and message.channel.id == 817409301479948338:
+    if message.content != "./rank" and message.channel.id == 817199997770137600:
         try:
             await message.author.send(f"You can only check your rank in {message.channel.mention}.")
             await message.delete()
@@ -25,6 +27,7 @@ async def on_message(message):
             pass
     else:
         await client.process_commands(message)
+        await client.handler.propagate(message)
         if "discord.gg/" in message.content.lower() or "discordapp.com/invite/" in message.content.lower():
             await message.author.send("sending server invites breaks the rules, if you think this is a mistake please contact the moderator.")
             await message.delete()
@@ -39,7 +42,7 @@ async def on_message(message):
             user = message.author
             user_id = str(message.author.id)
             guild_id = str(message.guild.id)
-            channel_ids = [813164947848167464, 814936857300566066]
+            channel_ids = [803586866619088946, 785614476241666048, 803591038349738014, 785910036953301033, 810641490014502972, 817199997770137600, 817769219898474536]
         except:
             pass
         if not user.bot and message.channel.id not in channel_ids:
